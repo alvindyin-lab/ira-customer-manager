7e0b7180de6f94543ecd8054bb64f09dc295455b
alvindyin-lab <alvindy.in@gmail.com>
2026-09-01 15:41:05 +0700

    Update layout.js

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -10,3 +10,21 @@ fetch('includes/topbar.html')
     .then(html => {
         document.getElementById('topbar').innerHTML = html;
     });
+
+fetch('includes/page-dashboard.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('dashboard').innerHTML = html;
+    });
+
+fetch('includes/page-pelanggan.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('pelanggan').innerHTML = html;
+    });
+
+fetch('includes/add-pelanggan.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('add-pelanggan').innerHTML = html;
+    });
